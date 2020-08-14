e493e140510b581790aa1ab321bb602b6cbfffab
Gisan Geff <ggranraniego@gmail.com>
2020-08-14 22:49:49 +0800

    Add validation

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -16,18 +16,25 @@ class TaskController extends Controller
     }
 
     public function store(Request $request) {
-        $task = Task::create($request->all());
+        $task = Task::create($request->all(), $this->validateTask());
 
         return $task;
     }
 
     public function update(Request $request, Task $task) {
-        $task->update($request->all());
-        return response()->json('Updated');
+        $task->update($request->all(), $this->validateTask());
+        return response()->json('Updated.');
     }
 
     public function destroy(Task $task) {
         $task->delete();
-        return response()->json('Deleted');
+        return response()->json('Deleted.');
+    }
+
+    public function validateTask() {
+        return request()->validate([
+            'title' => 'required',
+            'description' => 'required'
+        ]);
     }
 }
